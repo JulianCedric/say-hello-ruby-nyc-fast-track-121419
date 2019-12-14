e3146293736a089420618bd804a61daa15899864
Julian Pormentilla <julian.pormentilla@gmail.com>
2019-12-14 17:36:28 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,11 +1,5 @@
 # Build your say_hello method here
 
-# def say_hello
-#   puts "Hello Ruby programmer"
-# end
-
-# puts say_hello
-
 def say_hello(name = "Ruby programmer")
   puts "Hello #{name}!"
 end
